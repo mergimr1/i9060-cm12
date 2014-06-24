5c25862646fe9f19b3a26118594bee4cfd4b2826
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-06-24 22:46:11 +0530

    i9060 specific changes

--- cm.mk
+++ cm.mk
@@ -2,20 +2,18 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := GalaxyGrandDuos
+PRODUCT_RELEASE_NAME := GalaxyGrandNeo
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/i9082/device_i9082.mk)
+$(call inherit-product, device/samsung/i9060/device_i9060.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := i9082
-PRODUCT_NAME := cm_i9082
+PRODUCT_DEVICE := i9060
+PRODUCT_NAME := cm_i9060
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := GT-I9082
+PRODUCT_MODEL := GT-I9060
 
-#Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=baffinvj TARGET_DEVICE=baffin BUILD_FINGERPRINT=samsung/baffinvj/baffin:4.1.2/JZO54K/I9082LDCAMC2:user/release-keys PRIVATE_BUILD_DESC="baffinvj-user 4.1.2 JZO54K I9082LDCAMC2 release-keys"
