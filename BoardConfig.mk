d50f7b3a3812c55cbb70cb0f38c835a6f071cb1c
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-07-11 18:22:41 +0530

    Update BoardConfig.mk
    only a few changes made .. .more will be made according to the proprietary drivers

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,20 +1,20 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/samsung/i9082/BoardConfigVendor.mk
+-include vendor/samsung/i9060/BoardConfigVendor.mk
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := capri
+TARGET_BOARD_PLATFORM := java
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a9
 
-TARGET_BOOTLOADER_BOARD_NAME := capri
+TARGET_BOOTLOADER_BOARD_NAME := java
 
-BOARD_KERNEL_CMDLINE := console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000
-BOARD_KERNEL_BASE := 0xa2000000
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x81e00000
 BOARD_KERNEL_PAGESIZE := 4096
 
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -26,16 +26,16 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 4404019200
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_i9082_defconfig
-
+TARGET_KERNEL_CONFIG := cyanogenmod_i9060_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/i9060
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9082/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/i9082/libbt_vndcfg.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9060/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/i9060/libbt_vndcfg.txt
 
 # Connectivity - Wi-Fi
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
@@ -62,7 +62,7 @@ BOARD_USES_SKTEXTBOX := true
 
 # Hardware rendering
 USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := device/samsung/i9082/egl.cfg
+BOARD_EGL_CFG := device/samsung/i9060/egl.cfg
 BOARD_USE_MHEAP_SCREENSHOT := true
 BOARD_EGL_WORKAROUND_BUG_10194508 := true
 TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
@@ -77,24 +77,24 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 
 # RIL
-BOARD_RIL_CLASS := ../../../device/samsung/i9082/ril/
+BOARD_RIL_CLASS := ../../../device/samsung/i9060/ril/
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/samsung/i9082/fstab.capri_ss_baffin
+TARGET_RECOVERY_FSTAB := device/samsung/i9060/fstab.capri_ss_baffin
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
 
 # healthd
 BOARD_HAL_STATIC_LIBRARIES := libhealthd.capri
 
 # CMHW
-BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/i9082/cmhw/
+BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/i9060/cmhw/
 
 # GPS
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/i9082/include
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/i9060/include
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
-    device/samsung/i9082/sepolicy
+    device/samsung/i9060/sepolicy
 
 BOARD_SEPOLICY_UNION += \
     file_contexts \
