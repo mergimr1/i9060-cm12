e269d382ec98005be1049f8705be98d395713c4c
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-08-09 23:40:51 +0530

    final commit to change i9060 to baffinlite

--- cm.mk
+++ cm.mk
@@ -8,11 +8,11 @@ PRODUCT_RELEASE_NAME := GalaxyGrandNeo
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/i9060/device_i9060.mk)
+$(call inherit-product, device/samsung/baffinlite/device_baffinlite.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := i9060
-PRODUCT_NAME := cm_i9060
+PRODUCT_DEVICE := baffinlite
+PRODUCT_NAME := cm_baffinlite
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9060
--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 VENDOR=samsung
-DEVICE=i9060
+DEVICE=baffinlite
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 
